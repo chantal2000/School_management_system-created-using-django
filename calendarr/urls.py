5da51f1297b676fba86aff23daaffcaf1622075a
bob <nniyonkuruchantal@gmail.com>
2021-10-07 15:50:13 +0300

    commit

--- calendarr/urls.py
+++ calendarr/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     url('registerevent/',views.CalendarView.as_view(),name='calendar'),
     url('eventform/',views.event,name='eventform'),
-    url("list/",views.even,name="events_list"),
+    url("list/",views.events_list,name="events_list"),
     url("profile/<int:id>/",views.event_profile,name="event_profile"),
     url('home/',views.home_events,name="home_events"),
     url("edit/<int:id>/",views.edit_event,name="edit_event"),
